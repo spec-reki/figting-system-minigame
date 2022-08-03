87ba6edf42523b39aca3918a71f4bc574e6cdf91
-reki- <79382698+spec-reki@users.noreply.github.com>
2022-08-03 22:19:10 +0200

    Experiment started

--- Program.cs
+++ Program.cs
@@ -2,21 +2,27 @@
 
 namespace Fighting_System_Minigame
 {
-    class Player
+    class Player01
     {
-        
+        publlic string name;
+        public int hitpoints;
+
+        public Player01(string _name)
+        {
+            name = _name;
+        }
+    }
+
+    class Player02
+    {
+        publlic string name;
+        public int hitpoints;
     }
         
     class Program
     {
         static void Main (string []args)
         {
-
-
-
-
-
-
 
 
             Console.WriteLine("If you wanna play again press 1\n If you wonna left press 2");
